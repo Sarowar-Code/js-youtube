168704648e8b4fae5050f964f7e21a6d6cf90186
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-09 10:52:12 +0000

    operation, comparision, datatyopes

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,11 @@
+// Comparision we should avoid
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// Good comparision , which compare between same datatypes
+// console.log(3 > 2);
---
+++ 01_basics/06_datatypes.js
@@ -0,0 +1,48 @@
+//catagories based on storing...
+// Premetive
+// Non Premetive/ReferenceType
+
+
+/*
+
+>> Premetive datatypes 7 types
+
+>String
+>Number
+>Boolean
+>Null
+>Undefined
+>Symble
+>Bigint
+
+>> Reference type / Non Premetive 
+
+>Array
+>Object
+>Functions
+
+
+
+// let nundv
+
+// console.log(nundv); undefined
+
+
+
+>> Symble datatype
+
+
+const id = Symbol('123')
+const userId = Symbol('123')
+
+console.log(id === userId); false 
+
+
+
+>> BigInt datatype 
+
+const bigNumber = 2352363452653n // adding "n" is convert the number into bigInt
+
+console.log(typeof bigNumber);
+
+*/
