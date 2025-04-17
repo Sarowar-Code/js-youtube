db84ea6409b12f08bf2d92ed7ea48aae6fc97ae6
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-17 11:48:56 +0000

    "Array done"

--- referencedataType/09_array.js
+++ referencedataType/09_array.js
@@ -2,7 +2,7 @@
 
 const myArray = [0, 1, 2, 34, 53, 435]
 
-console.log(myArray);
+// console.log(myArray);
 
 // myArray.push(9)
 // myArray.pop()
@@ -15,9 +15,29 @@ console.log(myArray);
 
 // console.log(newArray);
 
-const slicearr = myArray.slice(2, 5)
-console.log("A", myArray);
+// const slicearr = myArray.slice(2, 5)
+// console.log("A", myArray);
 
-const slicearr2 = myArray.splice(2, 5)
-console.log("B", myArray);
+// const slicearr2 = myArray.splice(2, 5)
+// console.log("B", myArray);
+
+marvel_heroes = ["Iron Man", "Captain America", "Spider-Man", "Thor", "Hulk", "Doctor Strange"]
+
+dc_heroes = ["Superman", "Batman", "Flash", "Aquaman", "Green Lantern"]
+
+
+// marvel_heroes.push(dc_heroes) X
+
+// const all_heros = marvel_heroes.concat(dc_heroes) unMute main array.
+// ===
+// const all_heros = [...marvel_heroes, ...dc_heroes] same as concat methode , but short syntx
+
+const anotherArray = [1, 2, 4, [45, 65, 43], 3, 55, 3234, [34, 54, 554, [30, 45, 5]]]
+
+const flatArray = anotherArray.flat(2) // depth-2 [ 1,   2,  4, 45, 65, 43, 3, 55, 3234, 34, 54, 554, 30, 45, 5 ] 
+
+console.log(flatArray);
+
+
+// Next i sould learn Array.inArray | .from | .of 
 
