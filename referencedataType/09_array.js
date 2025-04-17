55e4c9fdeaa4f2b9632f63c5e89d042f3b7f3435
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-17 09:55:08 +0000

    "Array"

---
+++ referencedataType/09_array.js
@@ -0,0 +1,23 @@
+// Array
+
+const myArray = [0, 1, 2, 34, 53, 435]
+
+console.log(myArray);
+
+// myArray.push(9)
+// myArray.pop()
+
+// myArray.unshift(10)
+// myArray.shift()
+
+// console.log(myArray.indexOf(2) === 2);
+// const newArray = myArray.join()
+
+// console.log(newArray);
+
+const slicearr = myArray.slice(2, 5)
+console.log("A", myArray);
+
+const slicearr2 = myArray.splice(2, 5)
+console.log("B", myArray);
+
