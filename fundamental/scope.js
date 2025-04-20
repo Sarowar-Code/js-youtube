56530604d4ebecf72681ad00eaf1282dcafa37ea
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-20 15:57:56 +0000

    "scope"

--- fundamental/scope.js
+++ fundamental/scope.js
@@ -12,6 +12,28 @@ if(true){
 }
 
 
-console.log(a); // cannot print
-console.log(b); // cannot print
-console.log(c); // 30 >> usally var isn't maintain the scope. Thats the reason coders avoid using var.
+// console.log(a); // cannot print
+// console.log(b); // cannot print
+// console.log(c); // 30 >> usally var isn't maintain the scope. Thats the reason coders avoid using var.
+
+// +++++++++++++++++ Scope and Mini Hoisting +++++++++++++++++++++++ 
+
+if(true){
+    const username = "Sarowar"
+    if(username === "Sarowar"){
+        const website = "youtube"
+        console.log(username +" "+ website); // Sarowar youtube
+    }
+     // console.log(website); website is not defined
+}
+// console.log(username); username is not defined
+
+
+// ++++++++++++++++++ Intersting +++++++++++++++++++++++
+
+function addone (num){ 
+    return num + 1
+}
+
+addone(5)
+
