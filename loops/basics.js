b392fc2efb3c74bf0537e22b87cdfad9b35c0b61
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-28 15:59:50 +0000

    "loops"

---
+++ loops/basics.js
@@ -0,0 +1,24 @@
+// for 
+
+for (let i = 0; i <= 10; i++) {
+    if(i == 4){
+        // console.log(`detected ${i}`);
+        // break // lop will stops at this condition.
+        continue // it will spik 4 , 
+    }
+    // console.log(`num ${i}`);
+    
+}
+
+
+// While Loop
+
+let i = 0
+while (i <= 20) {
+    console.log(`index value is ${i}`);
+    i = i + 2
+}
+
+do {
+    
+} while (condition);
