cbcdb5d3ce3e6522aeeefe501239345a1f645650
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-29 15:38:11 +0000

    "filter & reduce & map"

--- loops/basics3.js
+++ loops/basics3.js
@@ -32,3 +32,38 @@ const books = [
 ];
   
 // map, filter, reduce // use forEach , forloop and practice ...
+
+
+
+// forEach is ideal when you want to do something with each element, not return something.
+books.forEach((item) => { 
+  // console.log(item.genre.toUpperCase()); 
+})
+
+
+// map()
+// map() creates a new array by applying a function to each element of the original array.
+
+
+const newBooks = books.map((item, index) => {
+  return { ...item , price : 100 * index + "$"} 
+
+})
+
+// console.log(newBooks);
+
+// filter
+// filter() creates a new array containing only the elements that pass a test (return true) from the provided function.
+const booksprice = books.filter((book)=> book.genre == "Fantasy" )
+// console.log(booksprice);
+
+
+// reduce()
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+const sum = numbers.reduce((acc, curr) => {
+  console.log(`acc :${acc}, curr: ${curr}`)
+  return acc + curr
+}, 0) // by default acc is 1. So we made this 0.
+
+console.log(sum)
+
