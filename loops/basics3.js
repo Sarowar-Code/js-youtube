f82410428ea27e90413a8898a58b35de3e5ee1c1
Sarowar Hossain <sarw838dev@gmail.com>
2025-04-29 12:51:25 +0000

    "es6 function"

---
+++ loops/basics3.js
@@ -0,0 +1,34 @@
+const books = [
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      year: 1960,
+      genre: "Fiction"
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      year: 1949,
+      genre: "Dystopian"
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      year: 1925,
+      genre: "Classic"
+    },
+    {
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      year: 1997,
+      genre: "Fantasy"
+    },
+    {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      year: 1937,
+      genre: "Fantasy"
+    }
+];
+  
+// map, filter, reduce // use forEach , forloop and practice ...
